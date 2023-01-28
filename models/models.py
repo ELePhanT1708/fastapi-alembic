c7bc9a4c39876fef534a8547e143a338fa627bfa
ELePhanT1708 <elephant1708@gmail.com>
2023-01-28 13:14:56 +0300

    Auth users was implemented with JWT cookies !

--- models/models.py
+++ models/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, Boolean
 
 metadata = MetaData()
 
@@ -16,9 +16,12 @@ user = Table(
     "user",
     metadata,
     Column("id", Integer, primary_key=True),
-    Column("email", String, nullable=False),
     Column("username", String, nullable=False),
-    Column("password", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.utcnow),
-    Column("role_id", Integer, ForeignKey("role.id")),
+    Column("role_id", Integer, ForeignKey(role.c.id)),
+    Column('email', String(length=320), unique=True, index=True, nullable=False),
+    Column('hashed_password', String(length=1024), nullable=False),
+    Column('is_active', Boolean, default=True, nullable=False),
+    Column('is_superuser', Boolean, default=False, nullable=False),
+    Column('is_verified', Boolean, default=False, nullable=False),
 )
